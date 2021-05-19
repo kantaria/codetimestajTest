e4d0d29d9570c827277108722c8ce36c451f7866
kantaria <kantariamo@yandex.ru>
2021-05-19 19:39:49 +0400

    pre commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync').create();
-const reload      = browserSync.reload;
-
+const
+const
+const gulp = require('gulp'),
+	minifyCSS = require('gulp-minify-css'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	prefix = require('gulp-autoprefixer'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync').create(),
+	reload      = browserSync.reload;
 // Save a reference to the `reload` method
 
 // Watch scss AND html files, doing different things with each.
@@ -15,4 +21,24 @@ gulp.task('serve', function () {
 	});
 
 	gulp.watch("*.html").on("change", reload);
+});
+
+// Minifies JS
+gulp.task('js', function(){
+	return gulp.src('js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'))
+});
+// CSS concat & minify
+gulp.task('styles', function(){
+	return gulp.src('css/**/*.css')
+		.pipe(prefix('last 2 versions'))
+		.pipe(concat('main.css'))
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('default', function() {
+	gulp.run('styles')
+	gulp.run('js')
 });
